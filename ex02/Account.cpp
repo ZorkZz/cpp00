b3e64c22b95e738413a367bc624b0ff0f50d4905
Alexandre Stachnik <astachni@z4r3p5.42lyon.fr>
2023-09-07 17:17:09 +0200

    a

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,18 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   account.cpp                                        :+:      :+:    :+:   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: astachni <astachni@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 13:25:56 by astachni          #+#    #+#             */
-/*   Updated: 2023/09/05 13:31:33 by astachni         ###   ########.fr       */
+/*   Updated: 2023/09/07 16:55:28 by astachni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
 #include <cstring>
+
+//getters
 
 int	Account::getNbAccounts(void)
 {
@@ -34,7 +36,15 @@ int	Account::getNbWithdrawals(void)
 	return (_totalNbWithdrawals);
 }
 
-void	Account::displayAccountsInfos(void)
+void	Account::makeDeposit(int deposit)
 {
-	std::cout << Account::getNbAccounts() << std::endl Account::
+	this->_nbDeposits++;
+	this->_amount += deposit;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	this->_nbWithdrawals++;
+	this->_totalNbWithdrawals += withdrawal;
+	return (0);
 }
